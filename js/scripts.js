94e5f6caf4dee7705d706202c2c37ad7b77cb31e
Anna Cushing <annamcushing@gmail.com>
2019-08-07 23:07:01 -0400

    burger animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,52 @@
 var $burger = $("#burger-icon");
 var $nav = $('nav[data-nav]');
 var $navA = $('nav[data-nav] a');
-var isVisible = false;
+// var isVisible = false;
 
-function mouseClick(){
+var $topLine = $('#topLine');
+var $middleLine = $('#middleLine');
+var $bottomLine = $('#bottomLine');
+var $burgerContainer = $('#burger-container');
+var animationSpeed = 0.3;
+var ifClicked = false;
 
-    console.log("click");
-    if(isVisible === false){
+function burgerClick(){
+    console.log("clicks");
+    if(ifClicked === false){
+        TweenMax.to($topLine,animationSpeed,{rotation:45, y:20, transformOrigin: "50%, 50%"});
+        TweenMax.to($middleLine,animationSpeed,{alpha:0, scaleX:0, transformOrigin: "50%, 50%"});
+        TweenMax.to($bottomLine,animationSpeed,{rotation:-45, y:-20, transformOrigin: "50%, 50%"});
         $nav.show();
-        isVisible = true;
-    }
-    else{
+        ifClicked = true;
+
+    }else{
+        TweenMax.to($topLine,animationSpeed,{rotation:0, y:0});
+        TweenMax.to($middleLine,animationSpeed,{alpha:1, scaleX:1});
+        TweenMax.to($bottomLine,animationSpeed,{rotation:0, y:0});
         $nav.hide();
-        isVisible = false;
+        ifClicked = false;
+
     }
 }
 
-$burger.on( "click", mouseClick);
-// close the mobile menu when menu link is clicked
-$navA.on( "click", mouseClick);
+//
+// function mouseClick(){
+//
+//     console.log("click");
+//     if(isVisible === false){
+//         $nav.show();
+//         isVisible = true;
+//     }
+//     else{
+//         $nav.hide();
+//         isVisible = false;
+//     }
+// }
+
+
+$burgerContainer.on("click", burgerClick);
+
+//
+// $burger.on( "click", mouseClick);
+// // close the mobile menu when menu link is clicked
+// $navA.on( "click", mouseClick);
